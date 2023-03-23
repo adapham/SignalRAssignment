640519925c94a53ce5ab52609c0f4745639a9c53
Khai Nguyen Ngoc (Intern-Data&AI-Technology-IVND-HO) <khai.nguyenngoc@vndirect.com.vn>
2023-03-23 21:03:29 +0700

    update paging

--- SignalRAssignment/Pages/Index.cshtml.cs
+++ SignalRAssignment/Pages/Index.cshtml.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SignalRAssignment.Common;
 using SignalRAssignment.Models;
-
 namespace SignalRAssignment.Pages
 {
     public class IndexModel : PageModel
@@ -19,11 +18,15 @@ namespace SignalRAssignment.Pages
         }
 
         public IList<Models.Product> Product { get; set; } = default!;
-
+     
         [FromQuery]
         [BindProperty(SupportsGet = true)]
         public string? SearchString { get; set; }
-
+        public const int ITEMS_PAGE = 10;
+        [BindProperty(SupportsGet =true,Name ="p")]
+        public int currentPage { get; set; }
+        public int countPages { get; set; }
+        public Func<int?, string> generateUrl { get; set; }
         public async Task OnGetAsync()
         {
             if (_context.Products != null)
@@ -47,13 +50,21 @@ namespace SignalRAssignment.Pages
                                .Where(p => p.ProductName.ToLower().Contains(SearchString.ToLower().Trim()))
                                .ToListAsync();
                     }
-
                 }
                 else
                 {
+                    int total = await _context.Products.CountAsync();
+                    countPages = (int)Math.Ceiling((double)total / ITEMS_PAGE);
+                    if(currentPage <1)
+                    {
+                        currentPage = 1;
+                    }
+
                     Product = await _context.Products
                         .Include(p => p.Category)
                         .Include(p => p.Supplier)
+                        .Skip(ITEMS_PAGE * (currentPage-1))
+                        .Take(ITEMS_PAGE)
                         .ToListAsync();
                 }
             }
