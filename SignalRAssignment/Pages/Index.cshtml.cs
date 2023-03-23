6c668953ea74580c4df4c04cf8e36cd683d7ba04
it.baonk <khacbao.1007@gmail.com>
2023-03-23 20:27:06 +0700

    update Bap

--- SignalRAssignment/Pages/Index.cshtml.cs
+++ SignalRAssignment/Pages/Index.cshtml.cs
@@ -19,35 +19,93 @@ namespace SignalRAssignment.Pages
         }
 
         public IList<Models.Product> Product { get; set; } = default!;
+        public IList<Models.Category> Categories { get; set; } = default!;
 
         [FromQuery]
         [BindProperty(SupportsGet = true)]
         public string? SearchString { get; set; }
+        [FromQuery]
+        [BindProperty(SupportsGet = true)]
+        public decimal? priceFrom { get; set; }
+        [FromQuery]
+        [BindProperty(SupportsGet = true)]
+        public decimal? priceTo { get; set; }
+        [FromQuery]
+        [BindProperty(SupportsGet = true)]
+        public int? cateId { get; set; }
 
         public async Task OnGetAsync()
         {
+            ////Xu ly category
+            if(_context.Categories != null)
+            {
+                Categories = await _context.Categories.ToListAsync();
+            }
+
+            if(cateId != null)
+            {
+                Product = await _context.Products
+                       .Include(p => p.Category)
+                       .Include(p => p.Supplier)
+                       .Where(p => p.CategoryId == cateId)
+                       .ToListAsync();
+                return;
+            }
+
+
             if (_context.Products != null)
             {
                 if (!string.IsNullOrEmpty(SearchString))
                 {
                     try
                     {
-                        var value = Decimal.Parse(SearchString);
-                        Product = await _context.Products
-                              .Include(p => p.Category)
-                              .Include(p => p.Supplier)
-                              .Where(p => p.UnitPrice == value || p.ProductId == (int)value)
-                              .ToListAsync();
+                        var valueSearch = decimal.Parse(SearchString);
+
+                        //Search theo price + proId
+                        if (priceFrom != null && priceTo != null)
+                        {
+                            //Search theo ProID
+                            Product = await _context.Products
+                                  .Include(p => p.Category)
+                                  .Include(p => p.Supplier)
+                                  .Where(p => p.UnitPrice == valueSearch || (p.ProductId == (int)valueSearch
+                                    && p.UnitPrice >= priceFrom && p.UnitPrice <= priceTo))
+                                  .ToListAsync();
+                        }
+                        else
+                        {
+                            //Search theo ProID
+                            Product = await _context.Products
+                                  .Include(p => p.Category)
+                                  .Include(p => p.Supplier)
+                                  .Where(p => p.UnitPrice == valueSearch || p.ProductId == (int)valueSearch)
+                                  .ToListAsync();
+                        }
+
                     }
                     catch (Exception)
                     {
-                        Product = await _context.Products
-                               .Include(p => p.Category)
-                               .Include(p => p.Supplier)
-                               .Where(p => p.ProductName.ToLower().Contains(SearchString.ToLower().Trim()))
-                               .ToListAsync();
+                        if (priceFrom != null && priceTo != null)
+                        {
+                            //Search theo Product Name + Price
+                            Product = await _context.Products
+                                   .Include(p => p.Category)
+                                   .Include(p => p.Supplier)
+                                   .Where(p => p.ProductName.ToLower().Contains(SearchString.ToLower().Trim())
+                                        && p.UnitPrice >= priceFrom && p.UnitPrice <= priceTo)
+                                   .ToListAsync();
+                        }
+                        else
+                        {
+                            //Search theo Product Name
+                            Product = await _context.Products
+                                   .Include(p => p.Category)
+                                   .Include(p => p.Supplier)
+                                   .Where(p => p.ProductName.ToLower().Contains(SearchString.ToLower().Trim()))
+                                   .ToListAsync();
+                        }
+                        
                     }
-
                 }
                 else
                 {
@@ -58,8 +116,6 @@ namespace SignalRAssignment.Pages
                 }
             }
         }
-
-
 
     }
 }
